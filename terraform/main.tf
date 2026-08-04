264cfdd23bc3927e79374d7a5ce2f044bd473f13
ElevenNotes <github@11notes.ch>
2026-08-04 07:35:52 +0200

    feat: add k8s ingress examples for routing

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,13 +22,17 @@ provider "helm" {
   }
 }
 
+variable "netbird_fqdn" {
+  type = string
+}
+
+variable "wildcard_fqdn" {
+  type = string
+}
+
 variable "postgres_password" {
   type = string
   sensitive = true
-}
-
-variable "netbird_fqdn" {
-  type = string
 }
 
 resource "kubernetes_namespace_v1" "netbird" {
@@ -48,6 +52,178 @@ resource "kubernetes_secret_v1" "postgres_password" {
   }
 
   type = "Opaque"
+}
+
+resource "kubernetes_ingress_v1" "netbird_ingress_grpc" {
+  metadata {
+    name      = "netbird-ingress-grpc"
+    namespace = "netbird"
+    annotations = {
+      "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
+      "traefik.ingress.kubernetes.io/service.serversscheme" = "h2c"
+    }
+  }
+
+  spec {
+    ingress_class_name = "traefik"
+
+    tls {
+      hosts = [trimspace(var.netbird_fqdn)]
+      secret_name = "wildcard-${replace(trimspace(var.wildcard_fqdn), ".", "-")}-tls"
+    }
+
+    rule {
+      host = trimspace(var.netbird_fqdn)
+      http {
+        path {
+          path = "/signalexchange.SignalExchange/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+        path {
+          path = "/management.ManagementService/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+        path {
+          path = "/management.ProxyService/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "netbird_ingress_ws" {
+  metadata {
+    name      = "netbird-ingress-ws"
+    namespace = "netbird"
+    annotations = {
+      "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
+    }
+  }
+
+  spec {
+    ingress_class_name = "traefik"
+
+    tls {
+      hosts = [trimspace(var.netbird_fqdn)]
+      secret_name = "wildcard-${replace(trimspace(var.wildcard_fqdn), ".", "-")}-tls"
+    }
+
+    rule {
+      host = trimspace(var.netbird_fqdn)
+      http {
+        path {
+          path      = "/relay"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+        path {
+          path      = "/ws-proxy/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+        path {
+          path      = "/api"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+        path {
+          path      = "/oauth2"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "netbird_ingress" {
+  metadata {
+    name      = "netbird-ingress"
+    namespace = "netbird"
+    annotations = {
+      "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
+    }
+  }
+
+  spec {
+    ingress_class_name = "traefik"
+
+    tls {
+      hosts = [trimspace(var.netbird_fqdn)]
+      secret_name = "wildcard-${replace(trimspace(var.wildcard_fqdn), ".", "-")}-tls"
+    }
+
+    rule {
+      host = trimspace(var.netbird_fqdn)
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "netbird-dashboard"
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 resource "helm_release" "netbird_db" {
@@ -82,12 +258,38 @@ resource "helm_release" "netbird_db" {
   ]
 }
 
+resource "helm_release" "netbird_dashboard" {
+  name = "netbird-dashboard"
+  repository = "oci://ghcr.io/11notes/charts"
+  chart = "netbird-dashboard"
+  namespace = "netbird"
+  version = "1.0.0"
+
+  values = [
+    yamlencode({
+      image = {
+        tag = "0.76.1"
+      }
+      netbird = {
+        fqdn = trimspace(var.netbird_fqdn)
+      }
+      persistence = {
+        var = {
+          size = "256Mi"
+        }
+      }
+    })
+  ]
+
+  depends_on = [helm_release.netbird_db]
+}
+
 resource "helm_release" "netbird" {
   name = "netbird"
   repository = "oci://ghcr.io/11notes/charts"
   chart = "netbird"
   namespace = "netbird"
-  version = "0.0.1"
+  version = "1.0.0"
 
   values = [
     yamlencode({
@@ -103,18 +305,15 @@ resource "helm_release" "netbird" {
         serviceName = "postgres"
       }
       persistence = {
-        netbird-etc = {
+        etc = {
           size = "16Mi"
         }
-        netbird-var = {
+        var = {
           size = "2Gi"
-        }
-        dashboard-var = {
-          size = "256Mi"
         }
       }
     })
   ]
 
-  depends_on = [helm_release.netbird_db]
+  depends_on = [helm_release.netbird_dashboard]
 }
